dc8f6da7982eca839c83f1c9ce3b8c6a790dd3e6
Jagdish <jagdish.g@simformsolutions.com>
2022-03-04 15:53:06 +0530

    created UI part

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,18 @@
+// Class Component
+
+import React from "react";
+
+import "./App.css";
+import TodoContainer from "./components/TodoContainer";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <TodoContainer></TodoContainer>
+      </>
+    );
+  }
+}
+
+export default App;
